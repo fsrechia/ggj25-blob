6e6f4957aaf7e97f1d4bafba77d98a2b0276b027
Felipe Rechia <felipe.rechia@gmail.com>
2025-01-25 19:16:06 -0300

    Add Pinion assets and enemy scene (not working)

---
+++ Scenes/enemy.gd
@@ -0,0 +1,13 @@
+extends CharacterBody3D
+
+@onready var armature = $Armature
+@onready var animationplayer = $AnimationPlayer
+
+
+func _ready() -> void:
+	# current_animation
+	pass
+	
+
+func _physics_process(delta: float) -> void:
+	pass
