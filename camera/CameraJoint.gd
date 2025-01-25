7c68e5dca48f5354a999d2c748b83475d33b5277
Felipe Rechia <felipe.rechia@gmail.com>
2025-01-25 09:49:37 -0300

    Add controller-based camera movement

--- camera/CameraJoint.gd
+++ camera/CameraJoint.gd
@@ -6,7 +6,23 @@ extends Node3D
 func _ready () -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	rotation_degrees.x = pitch_range.x / 5 * 4 + pitch_range.y / 5 * 1
-		
+
+static func get_camera_movement_input() -> Vector2:
+	var vector := Vector2(
+		Input.get_action_strength("cam_up") - Input.get_action_strength("cam_down"),
+		Input.get_action_strength("cam_left") - Input.get_action_strength("cam_right")
+	)
+	return vector
+
+func _process(delta: float) -> void:
+	#if event is InputEventJoypadMotion && (event.axis == JOY_AXIS_RIGHT_X or event.axis == JOY_AXIS_RIGHT_Y):
+	var cam_movement: Vector2 = get_camera_movement_input()
+	rotation_degrees.x -= cam_movement.x
+	rotation_degrees.x = clamp(rotation_degrees.x, pitch_range.x, pitch_range.y)
+	
+	rotation_degrees.y -=  cam_movement.y
+	rotation_degrees.y = wrapf(rotation_degrees.y, 0.0, 360.0)
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		rotation_degrees.x -= event.relative.y * mouse_sensitivity
