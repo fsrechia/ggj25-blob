60f94e0d5d425819bbe2662b7f1b845674433203
gbheimovski <gbashei@gmail.com>
2025-01-26 15:17:20 -0300

    upload bubble machine code

--- Scripts/BubbleMachine.gd
+++ Scripts/BubbleMachine.gd
@@ -5,7 +5,12 @@ var entered = false
 var items_delivered = 0
 var max_items = 3
 
+
 @onready var message3D = $Message3D
+@onready var WaterDelivered = $WaterDelivered
+@onready var SoapDelivered = $SoapDelivered
+@onready var CylinderDelivered = $CylinderDelivered
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,9 +23,17 @@ func _process(delta: float) -> void:
 		message3D.show()
 	else:
 		message3D.hide()
-	
 
 	if Input.is_action_just_pressed("interact") && activated:
+		print("Pressed")
+		#WaterDelivered.show()
+		#var timer : Timer = Timer.new()
+		#add_child(timer)
+		#timer.one_shot = true
+		#timer.autostart = false
+		#timer.wait_time = 2.0
+		#timer.timeout.connect(_timer_Timeout)
+		#timer.start()
 		queue_free()
 
 
@@ -28,25 +41,57 @@ func _process(delta: float) -> void:
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if "Player" in body.name:
 		entered = true
-		
 		if body.has_water:
 			body.has_water = false
 			#Play some anim/effect
 			items_delivered += 1
+			#WaterDelivered.show()
+			#var timer : Timer = Timer.new()
+			#add_child(timer)
+			#timer.one_shot = true
+			#timer.autostart = false
+			#timer.wait_time = 2.0
+			#timer.timeout.connect(_timer_Timeout)
+			#timer.start()
+			
 
 		if body.has_oxygen_cylinder:
 			body.has_oxygen_cylinder = false
 			items_delivered += 1
-			#Play some anim/effect
+			#CylinderDelivered.show()
+			##Play some anim/effect
+			#var timer2 : Timer = Timer.new()
+			#add_child(timer2)
+			#timer2.one_shot = true
+			#timer2.autostart = false
+			#timer2.wait_time = 2.0
+			#timer2.timeout.connect(_timer2_Timeout)
+			#timer2.start()
 		
 		if body.has_soap:
 			body.has_soap = false
 			items_delivered += 1
 			#Play some anim/effect
+			#SoapDelivered.show()
+			#var timer3 : Timer = Timer.new()
+			#add_child(timer3)
+			#timer3.one_shot = true
+			#timer3.autostart = false
+			#timer3.wait_time = 2.0
+			#timer3.timeout.connect(_timer3_Timeout)
+			#timer3.start()
 			
 		if items_delivered == max_items:
+			print("Activated")
 			activated = true
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
 	if "Player" in body.name:
 		entered = false
+
+func _timer_Timeout():
+	WaterDelivered.hide()
+func _timer2_Timeout():
+	CylinderDelivered.hide()
+func _timer3_Timeout():
+	SoapDelivered.hide()
