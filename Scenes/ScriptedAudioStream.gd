53c8810bf2ea90df37e348235c3de59906700606
Felipe Rechia <felipe.rechia@gmail.com>
2025-01-26 01:10:22 -0300

    Provide functions to swap synched songs easily

--- Scenes/ScriptedAudioStream.gd
+++ Scenes/ScriptedAudioStream.gd
@@ -8,14 +8,31 @@ const MUTE_VOLUME := -60
 var t = Timer.new()
 
 func _ready() -> void:
+	play_chill()
+
+func play_chill() -> void:
+	st.set_sync_stream_volume(global.SongState.CHILL,NORMAL_VOLUME)
+	st.set_sync_stream_volume(global.SongState.CLOCK,MUTE_VOLUME)
+	st.set_sync_stream_volume(global.SongState.TENSE,MUTE_VOLUME)
+	st.set_sync_stream_volume(global.SongState.TENSE_TRAP,MUTE_VOLUME)
+
+func play_clock() -> void:
+	st.set_sync_stream_volume(global.SongState.CHILL,MUTE_VOLUME)
+	st.set_sync_stream_volume(global.SongState.CLOCK,NORMAL_VOLUME)
+	st.set_sync_stream_volume(global.SongState.TENSE,MUTE_VOLUME)
+	st.set_sync_stream_volume(global.SongState.TENSE_TRAP,MUTE_VOLUME)
+
+func play_tense() -> void:
 	st.set_sync_stream_volume(global.SongState.CHILL,MUTE_VOLUME)
 	st.set_sync_stream_volume(global.SongState.CLOCK,MUTE_VOLUME)
 	st.set_sync_stream_volume(global.SongState.TENSE,NORMAL_VOLUME)
 	st.set_sync_stream_volume(global.SongState.TENSE_TRAP,MUTE_VOLUME)
 
-	t.start(5.0)
-
-
+func play_trap() -> void:
+	st.set_sync_stream_volume(global.SongState.CHILL,MUTE_VOLUME)
+	st.set_sync_stream_volume(global.SongState.CLOCK,MUTE_VOLUME)
+	st.set_sync_stream_volume(global.SongState.TENSE,MUTE_VOLUME)
+	st.set_sync_stream_volume(global.SongState.TENSE_TRAP,NORMAL_VOLUME)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
