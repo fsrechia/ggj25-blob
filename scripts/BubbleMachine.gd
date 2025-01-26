f95f4bbc26ebeb327e360ec048e620902988ead9
gbheimovski <gbashei@gmail.com>
2025-01-25 21:51:00 -0300

    Bubble machine working with a placeholder
    For now it is being destrcuted when do the interaction command only when collected all 3.

---
+++ scripts/BubbleMachine.gd
@@ -0,0 +1,45 @@
+extends Node3D
+
+var activated = false
+var entered = false
+var items_delivered = 0
+var max_items = 3
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#button deliver.visible = entered & !activated
+	if Input.is_action_just_pressed("interact") && activated:
+		queue_free()
+
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if "Player" in body.name:
+		entered = true
+		
+		if body.has_water:
+			body.has_water = false
+			#Play some anim/effect
+			items_delivered += 1
+
+		if body.has_oxygen_cylinder:
+			body.has_oxygen_cylinder = false
+			items_delivered += 1
+			#Play some anim/effect
+		
+		if body.has_soap:
+			body.has_soap = false
+			items_delivered += 1
+			#Play some anim/effect
+			
+		if items_delivered == max_items:
+			activated = true
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if "Player" in body.name:
+		entered = false
