299d363b413877250352943e68869f5ad8c646ce
gbheimovski <50345368+gbhsith@users.noreply.github.com>
2025-01-26 22:45:36 -0300

    Adding Credits Menu

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,6 +1,7 @@
 extends Node
 
 const PLANET_SCENE = preload("res://Scenes/PlanetScene.tscn")
+const CREDITS = preload("res://Scenes/Menus/Credits/Credits.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,7 +18,7 @@ func _on_play_pressed() -> void:
 
 
 func _on_credits_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_packed(CREDITS)
 
 
 func _on_quit_pressed() -> void:
