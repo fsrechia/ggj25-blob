cc90520fde29a94aadc710716eb17607bfe66ca3
Felipe Rechia <5440095+fsrechia@users.noreply.github.com>
2025-02-09 10:21:18 -0300

    Fix bubble scaling code

--- Scripts/AtmosphericBubble.gd
+++ Scripts/AtmosphericBubble.gd
@@ -1,9 +1,9 @@
 extends MeshInstance3D
 
-@export var initial_scale = Vector3(100.0,100.0,100.0)
+@export var bubble_initial_scale = Vector3(90.0,90.0,90.0)
 @export var final_global_position : = Vector3(0.0,0.0,0.0)
-@export var final_scale : = Vector3(110.0,110.0,110.0)
-@export var growth_speed = 105.0
+@export var final_scale : = Vector3(120.0,120.0,120.0)
+@export var growth_speed = 0.02
 
 @onready var bubble_growing = false 
 
@@ -11,27 +11,29 @@ extends MeshInstance3D
 func _ready() -> void:
 	print("Atmosphere initial scale ",self.get_scale())
 	print("Atmosphere initial size ",self.get_aabb().size)
-	self.set_scale(initial_scale)
+	self.set_scale(bubble_initial_scale)
 	print("Atmosphere scale set to ",self.get_scale())
-	start_growth()
+
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if bubble_growing:
-		grow_bubble_by(growth_speed * delta * Vector3(1.0,1.0,1.0))
+		var growth_vector = Vector3(1,1,1) + Vector3(1,1,1) * growth_speed * delta
+		# print("growth_vector ",growth_vector)
+		scale_bubble_by(growth_vector)
 
 func start_growth():
 	print("started growing the atmospheric bubble")
 	bubble_growing = true
 
-func grow_bubble_by(x):
-	var current_scale := self.get_scale()
-	current_scale += x
-	self.set_scale(self.get_scale())
-	print("Atmosphere scale set to current_scale ",current_scale)
-	if current_scale >= final_scale:
+func scale_bubble_by(new_scale):
+	var current_transform := self.get_transform()
+	self.set_transform(current_transform.scaled_local(new_scale))
+	#print("Atmosphere scale set to current_transform ",current_transform)
+	#print("Atmosphere scale set to get_scale ",self.get_scale())
+	if self.get_scale() >= final_scale:
 		print("stopped growing the atmospheric bubble")
 		bubble_growing = false
 	
